99a57d6d863ae147b364f46352d39e7fb795327b
Cheol <cheolkim02@gmail.com>
2025-08-12 21:00:18 +0900

    apartment prediction 1 finished

--- Apartment_Prediction_1/6_Model_Enhancement_2.py
+++ Apartment_Prediction_1/6_Model_Enhancement_2.py
@@ -74,3 +74,55 @@ rank 2: n_estimators = 10, max_depth = 32.
 
 저희는 이 두 모델을 사용해서 앙상블(Ensemble)을 진행하겠습니다 == Blending 기법
 '''
+first_model = RandomForestRegressor(n_estimators=10, max_depth=30, random_state=777)
+first_model.fit(train_x, train_y)
+
+second_model = RandomForestRegressor(n_estimators=10, max_depth=32, random_state=777)
+second_model.fit(train_x, train_y)
+
+# test 데이터 전처리
+test = pd.read_csv('test.csv')
+test['transaction_year'] = test['transaction_year_month'].astype(str).str[:4].astype(int)
+test['transaction_month'] = test['transaction_year_month'].astype(str).str[4:].astype(int)
+
+columns=['transaction_id', 'addr_kr', 'apartment_id', 'transaction_year_month']
+test_x = test.drop(columns=columns)
+
+test_x = test_x.replace({'transaction_date': '1~10'} , 1)
+test_x = test_x.replace({'transaction_date': '11~20'} , 2)
+test_x = test_x.replace({'transaction_date': '21~31'} , 3)
+test_x = test_x.replace({'transaction_date': '21~29'} , 3)
+test_x = test_x.replace({'transaction_date': '21~30'} , 3)
+test_x = test_x.replace({'transaction_date': '21~28'} , 3)
+
+import numpy as np
+for city in np.unique(test_x['city']):
+    if city not in le_city.classes_:
+        le_city.classes_ = np.append(le_city.classes_, city)
+test_x['city'] = le_city.transform(test_x['city'])
+
+for dong in np.unique(test_x['dong']):
+    if dong not in le_dong.classes_:
+        le_dong.classes_ = np.append(le_dong.classes_, dong)
+test_x['dong'] = le_dong.transform(test_x['dong'])
+
+for jibun in np.unique(test_x['jibun']):
+    if jibun not in le_jibun.classes_:
+        le_jibun.classes_ = np.append(le_jibun.classes_, jibun)
+test_x['jibun'] = le_jibun.transform(test_x['jibun'])
+
+for apt in np.unique(test_x['apt']):
+    if apt not in le_apt.classes_:
+        le_apt.classes_ = np.append(le_apt.classes_, apt)
+test_x['apt'] = le_apt.transform(test_x['apt'])
+
+# prediction
+prediction1 = first_model.predict(test_x)
+prediction2 = second_model.predict(test_x)
+
+'''# Ensemble - Blending (average)'''
+blended_prediction = prediction1*0.5 + prediction2*0.5
+
+submission = pd.read_csv('sample_submission.csv')
+submission['transaction_real_price'] = blended_prediction
+submission.to_csv('submission.csv', index=False)
